0c7eea51a056ab92daa3289155f2305915a05828
koutsilis1999 <kkoutsilis@gmail.com>
2022-02-06 18:08:35 +0000

    update app

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,55 @@
 package org.example;
 
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+import org.example.distance.EuclideanDistance;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+import static java.util.stream.Collectors.toSet;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        ArrayList<List<String>> recordlist = new ArrayList<>();
+//        List<Record> records =  new ArrayList<>();
+        try (CSVReader csvReader = new CSVReader(new FileReader("src/main/java/org/example/files/sample.csv"));) {
+            String[] values = null;
+
+            while ((values = csvReader.readNext()) != null) {
+                recordlist.add(Arrays.asList(values));
+            }
+        } catch (IOException | CsvValidationException e) {
+            e.printStackTrace();
+        }
+
+        List<Record> records = new ArrayList<>();
+        List<String> des = recordlist.get(0);
+        recordlist.remove(0);
+        for (List<String> strings : recordlist) {
+            Map<String,Double> test = new HashMap<>();
+            for (int j = 0; j < strings.size(); j++) {
+                test.put(des.get(j).toString(),Double.parseDouble(strings.get(j)));
+
+            }
+            records.add(new Record(des.toString(),test));
+        }
+//
+        Map<Centroid, List<Record>> clusters = KMeans.fit(records, 7, new EuclideanDistance(), 1000);
+        // Printing the cluster configuration
+        clusters.forEach((key, value) -> {
+            System.out.println("-------------------------- CLUSTER ----------------------------");
+            System.out.println(key);
+            String members = String.join(", ", value.stream().map(Record::getDescription).collect(toSet()));
+            System.out.print(members);
+            System.out.println();
+        });
+
     }
 }
